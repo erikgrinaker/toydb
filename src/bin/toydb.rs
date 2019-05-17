499dc0f2f7ec5e8dc2d3761d8f69ce75a386e6ca
Erik Grinaker <erik@grinaker.org>
2019-05-17 18:32:12 +0200

    Added command line arguments

--- src/bin/toydb.rs
+++ src/bin/toydb.rs
@@ -1,15 +1,26 @@
 #![warn(clippy::all)]
 
+#[macro_use]
+extern crate clap;
 extern crate config;
 extern crate serde;
+#[macro_use]
+extern crate serde_derive;
 extern crate simplelog;
 extern crate toydb;
 
-#[macro_use]
-extern crate serde_derive;
-
 fn main() -> Result<(), toydb::Error> {
-    let cfg = Config::new()?;
+    let opts = app_from_crate!()
+        .arg(
+            clap::Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .help("Configuration file path")
+                .takes_value(true)
+                .default_value("/etc/toydb.yaml"),
+        )
+        .get_matches();
+    let cfg = Config::new(opts.value_of("config").unwrap())?;
     simplelog::SimpleLogger::init(
         cfg.log_level.parse::<simplelog::LevelFilter>()?,
         simplelog::Config::default(),
@@ -26,14 +37,14 @@ struct Config {
 }
 
 impl Config {
-    fn new() -> Result<Self, config::ConfigError> {
+    fn new(file: &str) -> Result<Self, config::ConfigError> {
         let mut c = config::Config::new();
         c.set_default("id", "toydb")?;
         c.set_default("listen", "0.0.0.0:9605")?;
         c.set_default("threads", 4)?;
         c.set_default("log_level", "info")?;
 
-        c.merge(config::File::with_name("config/toydb.yaml"))?;
+        c.merge(config::File::with_name(file))?;
         c.merge(config::Environment::with_prefix("TOYDB"))?;
         c.try_into()
     }
