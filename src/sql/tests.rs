3c07f564abb14c14ef9a218ae89635758dcfc96c
Erik Grinaker <erik@grinaker.org>
2019-12-16 19:17:39 +0100

    sql: added support for errors in expression tests

--- src/sql/tests.rs
+++ src/sql/tests.rs
@@ -10,18 +10,24 @@ use crate::Error;
 use goldenfile::Mint;
 use std::io::Write;
 
+fn eval_expr(expr: &str) -> Result<Value, Error> {
+    let engine = super::engine::KV::new(kv::MVCC::new(kv::storage::Memory::new()));
+    let mut txn = engine.begin()?;
+    let ctx = Context { txn: &mut txn };
+    let ast = Parser::new(&format!("SELECT {}", expr)).parse()?;
+    let mut result = Plan::build(ast)?.optimize()?.execute(ctx)?;
+    let value = result.next().unwrap().unwrap().get(0).unwrap().clone();
+    txn.rollback()?;
+    Ok(value)
+}
+
 macro_rules! test_expr {
-    ( $( $name:ident: $expr:expr => $value:expr, )* ) => {
+    ( $( $name:ident: $expr:expr => $result:expr, )* ) => {
     $(
         #[test]
         fn $name() -> Result<(), Error> {
-            let engine = super::engine::KV::new(kv::MVCC::new(kv::storage::Memory::new()));
-            let mut txn = engine.begin()?;
-            let ctx = Context{txn: &mut txn};
-            let ast = Parser::new(&format!("SELECT {}", $expr)).parse()?;
-            let mut result = Plan::build(ast)?.optimize()?.execute(ctx)?;
-            txn.rollback()?;
-            assert_eq!($value, *result.next().unwrap()?.get(0).unwrap());
+            let value = eval_expr($expr);
+            assert_eq!($result, value);
             Ok(())
         }
     )*
@@ -239,58 +245,59 @@ test_sql! {
 use Value::*;
 
 test_expr! {
-    lit_bool_false: "FALSE" => Boolean(false),
-    lit_bool_true: "TRUE" => Boolean(true),
-    lit_float: "3.72" => Float(3.72),
-    lit_float_exp: "3.14e3" => Float(3140.0),
-    lit_float_exp_neg: "2.718E-2" => Float(0.02718),
-    lit_float_nodecimal: "3.0" => Float(3.0),
-    lit_int: "3" => Integer(3),
-    lit_int_multidigit: "314" => Integer(314),
-    lit_int_zeroprefix: "03" => Integer(3),
-    lit_null: "NULL" => Null,
-    lit_str: "'Hi! 👋'" => String("Hi! 👋".into()),
-    lit_str_quotes: r#"'Has ''single'' and "double" quotes'"# => String(r#"Has 'single' and "double" quotes"#.into()),
+    lit_bool_false: "FALSE" => Ok(Boolean(false)),
+    lit_bool_true: "TRUE" => Ok(Boolean(true)),
+    lit_float: "3.72" => Ok(Float(3.72)),
+    lit_float_exp: "3.14e3" => Ok(Float(3140.0)),
+    lit_float_exp_neg: "2.718E-2" => Ok(Float(0.02718)),
+    lit_float_nodecimal: "3.0" => Ok(Float(3.0)),
+    lit_int: "3" => Ok(Integer(3)),
+    lit_int_multidigit: "314" => Ok(Integer(314)),
+    lit_int_zeroprefix: "03" => Ok(Integer(3)),
+    lit_null: "NULL" => Ok(Null),
+    lit_str: "'Hi! 👋'" => Ok(String("Hi! 👋".into())),
+    lit_str_quotes: r#"'Has ''single'' and "double" quotes'"# => Ok(String(r#"Has 'single' and "double" quotes"#.into())),
 
-    op_add: "1 + 2" => Integer(3),
-    op_add_int_float: "1 + 3.72" => Float(1.0 + 3.72),
-    op_add_float_float: "3.1 + 2.71" => Float(3.1 + 2.71),
-    op_add_float_int: "3.72 + 1" => Float(1.0 + 3.72),
-    op_add_negative: "1 + -3" => Integer(-2),
+    op_add: "1 + 2" => Ok(Integer(3)),
+    op_add_int_float: "1 + 3.72" => Ok(Float(1.0 + 3.72)),
+    op_add_float_float: "3.1 + 2.71" => Ok(Float(3.1 + 2.71)),
+    op_add_float_int: "3.72 + 1" => Ok(Float(3.72 + 1.0)),
+    op_add_negative: "1 + -3" => Ok(Integer(-2)),
+    op_add_error_strings: "'a' + 'b'" => Err(Error::Value("Can't add a and b".into())),
 
-    op_assert: "+1" => Integer(1),
-    op_assert_float: "+3.72" => Float(3.72),
-    op_assert_multi: "+++1" => Integer(1),
+    op_assert: "+1" => Ok(Integer(1)),
+    op_assert_float: "+3.72" => Ok(Float(3.72)),
+    op_assert_multi: "+++1" => Ok(Integer(1)),
 
-    op_eq_bool_false_false: "FALSE = FALSE" => Boolean(true),
-    op_eq_bool_true_false: "TRUE = FALSE" => Boolean(false),
-    op_eq_bool_true_true: "TRUE = TRUE" => Boolean(true),
-    op_eq_float: "3.14 = 3.14" => Boolean(true),
-    op_eq_float_neq: "3.14 = 2.718" => Boolean(false),
-    op_eq_float_int: "3.0 = 3" => Boolean(true),
-    op_eq_float_int_neq: "3.01 = 3" => Boolean(false),
-    op_eq_int: "1 = 1" => Boolean(true),
-    op_eq_int_neq: "1 = 2" => Boolean(false),
-    op_eq_str: "'abc' = 'abc'" => Boolean(true),
-    op_eq_str_case: "'abc' = 'ABC'" => Boolean(false),
-    op_eq_str_neq: "'abc' = 'xyz'" => Boolean(false),
+    op_eq_bool_false_false: "FALSE = FALSE" => Ok(Boolean(true)),
+    op_eq_bool_true_false: "TRUE = FALSE" => Ok(Boolean(false)),
+    op_eq_bool_true_true: "TRUE = TRUE" => Ok(Boolean(true)),
+    op_eq_float: "3.14 = 3.14" => Ok(Boolean(true)),
+    op_eq_float_neq: "3.14 = 2.718" => Ok(Boolean(false)),
+    op_eq_float_int: "3.0 = 3" => Ok(Boolean(true)),
+    op_eq_float_int_neq: "3.01 = 3" => Ok(Boolean(false)),
+    op_eq_int: "1 = 1" => Ok(Boolean(true)),
+    op_eq_int_neq: "1 = 2" => Ok(Boolean(false)),
+    op_eq_str: "'abc' = 'abc'" => Ok(Boolean(true)),
+    op_eq_str_case: "'abc' = 'ABC'" => Ok(Boolean(false)),
+    op_eq_str_neq: "'abc' = 'xyz'" => Ok(Boolean(false)),
 
-    op_negate: "-1" => Integer(-1),
-    op_negate_double: "--1" => Integer(1),
-    op_negate_float: "-3.72" => Float(-3.72),
-    op_negate_mixed: "-+-+-1" => Integer(-1),
-    op_negate_multi: "---1" => Integer(-1),
+    op_negate: "-1" => Ok(Integer(-1)),
+    op_negate_double: "--1" => Ok(Integer(1)),
+    op_negate_float: "-3.72" => Ok(Float(-3.72)),
+    op_negate_mixed: "-+-+-1" => Ok(Integer(-1)),
+    op_negate_multi: "---1" => Ok(Integer(-1)),
 
-    op_neq_bool_false_false: "FALSE != FALSE" => Boolean(false),
-    op_neq_bool_true_false: "TRUE != FALSE" => Boolean(true),
-    op_neq_bool_true_true: "TRUE != TRUE" => Boolean(false),
-    op_neq_float: "3.14 != 2.718" => Boolean(true),
-    op_neq_float_eq: "3.14 != 3.14" => Boolean(false),
-    op_neq_float_int: "3.01 != 3" => Boolean(true),
-    op_neq_float_int_eq: "3.0 != 3" => Boolean(false),
-    op_neq_int: "1 != 2" => Boolean(true),
-    op_neq_int_eq: "1 != 1" => Boolean(false),
-    op_neq_str: "'abc' != 'xyz'" => Boolean(true),
-    op_neq_str_case: "'abc' != 'ABC'" => Boolean(true),
-    op_neq_str_eq: "'abc' != 'abc'" => Boolean(false),
+    op_neq_bool_false_false: "FALSE != FALSE" => Ok(Boolean(false)),
+    op_neq_bool_true_false: "TRUE != FALSE" => Ok(Boolean(true)),
+    op_neq_bool_true_true: "TRUE != TRUE" => Ok(Boolean(false)),
+    op_neq_float: "3.14 != 2.718" => Ok(Boolean(true)),
+    op_neq_float_eq: "3.14 != 3.14" => Ok(Boolean(false)),
+    op_neq_float_int: "3.01 != 3" => Ok(Boolean(true)),
+    op_neq_float_int_eq: "3.0 != 3" => Ok(Boolean(false)),
+    op_neq_int: "1 != 2" => Ok(Boolean(true)),
+    op_neq_int_eq: "1 != 1" => Ok(Boolean(false)),
+    op_neq_str: "'abc' != 'xyz'" => Ok(Boolean(true)),
+    op_neq_str_case: "'abc' != 'ABC'" => Ok(Boolean(true)),
+    op_neq_str_eq: "'abc' != 'abc'" => Ok(Boolean(false)),
 }
