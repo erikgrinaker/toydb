407cf7dd82a37cfb7fa5f1935dd0439610514104
Erik Grinaker <erik@grinaker.org>
2020-05-15 14:08:24 +0200

    clusters: run with --release flag

--- clusters/local/run.sh
+++ clusters/local/run.sh
@@ -2,10 +2,10 @@
 
 set -euo pipefail
 
-cargo build --bin toydb
+cargo build --release --bin toydb
 
 for ID in a b c d e; do
-    (cargo run -q -- -c toydb-$ID/toydb.yaml 2>&1 | sed -e "s/\\(.*\\)/toydb-$ID \\1/g") &
+    (cargo run -q --release -- -c toydb-$ID/toydb.yaml 2>&1 | sed -e "s/\\(.*\\)/toydb-$ID \\1/g") &
 done
 
 trap 'kill $(jobs -p)' EXIT
