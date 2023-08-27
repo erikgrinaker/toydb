b220a751dfc3ac77643a3ef5f35a0ad55593710e
Erik Grinaker <erik@grinaker.org>
2023-08-27 13:30:13 +0200

    Improve Store test cases.

--- src/storage/kv/mod.rs
+++ src/storage/kv/mod.rs
@@ -95,64 +95,86 @@ pub type Scan<'a> = Box<dyn DoubleEndedIterator<Item = Result<(Vec<u8>, Vec<u8>)
 
 #[cfg(test)]
 mod tests {
+    /// Generates common tests for any Store implementation.
     macro_rules! test_store {
         ($setup:expr) => {
+            /// Tests Store point operations, i.e. set, get, and delete.
             #[test]
-            fn delete() -> Result<()> {
+            fn point_ops() -> Result<()> {
                 let mut s = $setup;
+
+                // Getting a missing key should return None.
+                assert_eq!(None, s.get(b"a")?);
+
+                // Setting and getting a key should return its value.
                 s.set(b"a", vec![0x01])?;
                 assert_eq!(Some(vec![0x01]), s.get(b"a")?);
+
+                // Setting a different key should not affect the first.
+                s.set(b"b", vec![0x02])?;
+                assert_eq!(Some(vec![0x02]), s.get(b"b")?);
+                assert_eq!(Some(vec![0x01]), s.get(b"a")?);
+
+                // Getting a different missing key should return None. The
+                // comparison is case-insensitive for strings.
+                assert_eq!(None, s.get(b"c")?);
+                assert_eq!(None, s.get(b"A")?);
+
+                // Setting an existing key should replace its value.
+                s.set(b"a", vec![0x00])?;
+                assert_eq!(Some(vec![0x00]), s.get(b"a")?);
+
+                // Deleting a key should remove it, but not affect others.
                 s.delete(b"a")?;
                 assert_eq!(None, s.get(b"a")?);
-                s.delete(b"b")?;
+                assert_eq!(Some(vec![0x02]), s.get(b"b")?);
+
+                // Deletes are idempotent.
+                s.delete(b"a")?;
+                assert_eq!(None, s.get(b"a")?);
+
                 Ok(())
             }
 
             #[test]
-            fn get() -> Result<()> {
+            /// Tests Store point operations on empty keys and values.
+            /// These are as valid as any other key/value.
+            fn point_ops_empty() -> Result<()> {
                 let mut s = $setup;
-                s.set(b"a", vec![0x01])?;
-                assert_eq!(Some(vec![0x01]), s.get(b"a")?);
-                assert_eq!(None, s.get(b"b")?);
+
+                assert_eq!(None, s.get(b"")?);
+                s.set(b"", vec![])?;
+                assert_eq!(Some(vec![]), s.get(b"")?);
+                s.delete(b"")?;
+                assert_eq!(None, s.get(b"")?);
+
                 Ok(())
             }
 
             #[test]
-            fn random() -> Result<()> {
-                use rand::Rng;
+            /// Tests Store point operations on keys and values of
+            /// increasing sizes, up to 16 MB.
+            fn point_ops_sizes() -> Result<()> {
                 let mut s = $setup;
-                let mut rng: rand::rngs::StdRng = rand::SeedableRng::seed_from_u64(397_427_893);
 
-                // Create a bunch of random items and insert them
-                let mut items: Vec<(Vec<u8>, Vec<u8>)> = Vec::new();
-                for i in 0..1000_u64 {
-                    items.push((rng.gen::<[u8; 32]>().to_vec(), i.to_be_bytes().to_vec()))
-                }
-                for (key, value) in items.iter() {
+                // Generate keys/values for increasing powers of two.
+                for size in (1..=24).map(|i| 1 << i) {
+                    let bytes = "x".repeat(size);
+                    let key = bytes.as_bytes();
+                    let value = bytes.clone().into_bytes();
+
+                    assert_eq!(None, s.get(key)?);
                     s.set(key, value.clone())?;
+                    assert_eq!(Some(value), s.get(key)?);
+                    s.delete(key)?;
+                    assert_eq!(None, s.get(key)?);
                 }
-
-                // Fetch the random items, both via get() and scan()
-                for (key, value) in items.iter() {
-                    assert_eq!(s.get(key)?, Some(value.clone()))
-                }
-                let mut expect = items.clone();
-                expect.sort_by(|a, b| a.0.cmp(&b.0));
-                assert_eq!(expect, s.scan(Range::from(..)).collect::<Result<Vec<_>>>()?);
-                expect.reverse();
-                assert_eq!(expect, s.scan(Range::from(..)).rev().collect::<Result<Vec<_>>>()?);
-
-                // Remove the items
-                for (key, _) in items {
-                    s.delete(&key)?;
-                    assert_eq!(None, s.get(&key)?);
-                }
-                assert!(s.scan(Range::from(..)).collect::<Result<Vec<_>>>()?.is_empty());
 
                 Ok(())
             }
 
             #[test]
+            /// Tests various Store scans.
             fn scan() -> Result<()> {
                 let mut s = $setup;
                 s.set(b"a", vec![0x01])?;
@@ -223,12 +245,37 @@ mod tests {
             }
 
             #[test]
-            fn set() -> Result<()> {
+            fn random() -> Result<()> {
+                use rand::Rng;
                 let mut s = $setup;
-                s.set(b"a", vec![0x01])?;
-                assert_eq!(Some(vec![0x01]), s.get(b"a")?);
-                s.set(b"a", vec![0x02])?;
-                assert_eq!(Some(vec![0x02]), s.get(b"a")?);
+                let mut rng: rand::rngs::StdRng = rand::SeedableRng::seed_from_u64(397_427_893);
+
+                // Create a bunch of random items and insert them
+                let mut items: Vec<(Vec<u8>, Vec<u8>)> = Vec::new();
+                for i in 0..1000_u64 {
+                    items.push((rng.gen::<[u8; 32]>().to_vec(), i.to_be_bytes().to_vec()))
+                }
+                for (key, value) in items.iter() {
+                    s.set(key, value.clone())?;
+                }
+
+                // Fetch the random items, both via get() and scan()
+                for (key, value) in items.iter() {
+                    assert_eq!(s.get(key)?, Some(value.clone()))
+                }
+                let mut expect = items.clone();
+                expect.sort_by(|a, b| a.0.cmp(&b.0));
+                assert_eq!(expect, s.scan(Range::from(..)).collect::<Result<Vec<_>>>()?);
+                expect.reverse();
+                assert_eq!(expect, s.scan(Range::from(..)).rev().collect::<Result<Vec<_>>>()?);
+
+                // Remove the items
+                for (key, _) in items {
+                    s.delete(&key)?;
+                    assert_eq!(None, s.get(&key)?);
+                }
+                assert!(s.scan(Range::from(..)).collect::<Result<Vec<_>>>()?.is_empty());
+
                 Ok(())
             }
         };
