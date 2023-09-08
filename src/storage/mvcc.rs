0b09ad98929e64405497498a8ee3d0f2cba42076
Erik Grinaker <erik@grinaker.org>
2023-09-08 20:17:49 +0200

    Add MVCC module documentation.

--- src/storage/mvcc.rs
+++ src/storage/mvcc.rs
@@ -1,3 +1,144 @@
+//! This module implements MVCC (Multi-Version Concurrency Control), a widely
+//! used method for ACID transactions and concurrency control. It allows
+//! multiple concurrent transactions to access and modify the same dataset,
+//! isolates them from each other, detects and handles conflicts, and commits
+//! their writes atomically as a single unit. It uses an underlying storage
+//! engine to store raw keys and values.
+//!
+//! VERSIONS
+//! ========
+//!
+//! MVCC handles concurrency control by managing multiple historical versions of
+//! keys, identified by a timestamp. Every write adds a new version at a higher
+//! timestamp, with deletes having a special tombstone value. For example, the
+//! keys a,b,c,d may have the following values at various logical timestamps (x
+//! is tombstone):
+//!
+//! Time
+//! 5
+//! 4  a4          
+//! 3      b3      x
+//! 2            
+//! 1  a1      c1  d1
+//!    a   b   c   d   Keys
+//!
+//! A transaction t2 that started at T=2 will see the values a=a1, c=c1, d=d1. A
+//! different transaction t5 running at T=5 will see a=a4, b=b3, c=c1.
+//!
+//! ToyDB uses logical timestamps with a sequence number stored in
+//! Key::NextVersion. Each new read-write transaction takes its timestamp from
+//! the current value of Key::NextVersion and then increments the value for the
+//! next transaction.
+//!
+//! ISOLATION
+//! =========
+//!
+//! MVCC provides an isolation level called snapshot isolation. Briefly,
+//! transactions see a consistent snapshot of the database state as of their
+//! start time. Writes made by concurrent or subsequent transactions are never
+//! visible to it. If two concurrent transactions write to the same key they
+//! will conflict and one of them must retry. A transaction's writes become
+//! atomically visible to subsequent transactions only when they commit, and are
+//! rolled back on failure. Read-only transactions never conflict with other
+//! transactions.
+//!
+//! Transactions write new versions at their timestamp, storing them as
+//! Key::Version(key, version) => value. If a transaction writes to a key and
+//! finds a newer version, it returns an error and the client must retry.
+//!
+//! Active (uncommitted) read-write transactions record their version in the
+//! active set, stored as Key::Active(version). When new transactions begin, they
+//! take a snapshot of this active set, and any key versions that belong to a
+//! transaction in the active set are considered invisible (to anyone except that
+//! transaction itself). Writes to keys that already have a past version in the
+//! active set will also return an error.
+//!
+//! To commit, a transaction simply deletes its record in the active set. This
+//! will immediately (and, crucially, atomically) make all of its writes visible
+//! to subsequent transactions, but not ongoing ones. If the transaction is
+//! cancelled and rolled back, it maintains a record of all keys it wrote as
+//! Key::TxnWrite(version, key), so that it can find the corresponding versions
+//! and delete them before removing itself from the active set.
+//!
+//! Consider the following example, where we have two ongoing transactions at
+//! time T=2 and T=5, with some writes that are not yet committed marked in
+//! parentheses.
+//!
+//! Active set: [2, 5]
+//!
+//! Time
+//! 5 (a5)
+//! 4  a4          
+//! 3      b3      x
+//! 2         (x)     (e2)
+//! 1  a1      c1  d1
+//!    a   b   c   d   e   Keys
+//!
+//! Here, t2 will see a=a1, d=d1, e=e2 (it sees its own writes). t5 will see
+//! a=a5, b=b3, c=c1. t2 does not see any newer versions, and t5 does not see
+//! the tombstone at c@2 nor the value e=e2, because version=2 is in its active
+//! set.
+//!
+//! If t2 tries to write b=b2, it receives an error and must retry, because a
+//! newer version exists. Similarly, if t5 tries to write e=e5, it receives an
+//! error and must retry, because the version e=e2 is in its active set.
+//!
+//! To commit, t2 can remove itself from the active set. A new transaction t6
+//! starting after the commit will then see c as deleted and e=e2. t5 will still
+//! not see any of t2's writes, because it's still in its local snapshot of the
+//! active set at the time it began.
+//!
+//! READ-ONLY AND TIME TRAVEL QUERIES
+//! =================================
+//!
+//! Since MVCC stores historical versions, it can trivially support time travel
+//! queries where a transaction reads at a past timestamp and has a consistent
+//! view of the database at that time.
+//!
+//! This is done by a transaction simply using a past version, as if it had
+//! started far in the past, ignoring newer versions like any other transaction.
+//! This transaction cannot write, as it does not have a unique timestamp (the
+//! original read-write transaction originally owned this timestamp).
+//!
+//! The only wrinkle is that the time-travel query must also know what the active
+//! set was at that version. Otherwise, it may see past transactions that committed
+//! after that time, which were not visible to the original transaction that wrote
+//! at that version. Similarly, if a time-travel query reads at a version that is
+//! still active, it should not see its in-progress writes, and after it commits
+//! a different time-travel query should not see those writes either, to maintain
+//! version consistency.
+//!
+//! To achieve this, every read-write transaction stores its active set snapshot
+//! in the storage engine as well, as Key::TxnActiveSnapshot, such that later
+//! time-travel queries can restore its original snapshot. Furthermore, a
+//! time-travel query can only see versions below the snapshot version, otherwise
+//! it could see spurious in-progress or since-committed versions.
+//!
+//! In the following example, a time-travel query at version=3 would see a=a1,
+//! c=c1, d=d1.
+//!
+//! Time
+//! 5
+//! 4  a4          
+//! 3      b3      x
+//! 2            
+//! 1  a1      c1  d1
+//!    a   b   c   d   Keys
+//!
+//! Read-only queries work similarly to time-travel queries, with one exception:
+//! they read at the next (current) version, i.e. Key::NextVersion, and use the
+//! current active set, storing the snapshot in memory only. Read-only queries
+//! do not increment the version sequence number in Key::NextVersion.
+//!
+//! GARBAGE COLLECTION
+//! ==================
+//!
+//! Normally, old versions would be garbage collected regularly, when they are
+//! no longer needed by active transactions or time-travel queries. However,
+//! ToyDB does not implement garbage collection, instead keeping all history
+//! forever, both out of laziness and also because it allows unlimited time
+//! travel queries (it's a feature, not a bug!).
+
 use super::{keycode, Engine};
 use crate::error::{Error, Result};
 
@@ -40,7 +181,8 @@ enum Key<'a> {
     ),
     /// Unversioned non-transactional key/value pairs. These exist separately
     /// from versioned keys, i.e. the unversioned key "foo" is entirely
-    /// independent of the versioned key "foo@7".
+    /// independent of the versioned key "foo@7". These are mostly used
+    /// for metadata.
     Unversioned(
         #[serde(with = "serde_bytes")]
         #[serde(borrow)]
@@ -80,9 +222,16 @@ impl<'a> KeyPrefix<'a> {
     }
 }
 
-/// An MVCC-based transactional key-value engine.
+/// An MVCC-based transactional key-value engine. It wraps an underlying storage
+/// engine that's used for raw key/value storage.
+///
+/// While it supports any number of concurrent transactions, individual read or
+/// write operations are executed sequentially, serialized via a mutex. There
+/// are two reasons for this: the storage engine itself is not thread-safe,
+/// requiring serialized access, and the Raft state machine that manages the
+/// MVCC engine applies commands one at a time from the Raft log, which will
+/// serialize them anyway.
 pub struct MVCC<E: Engine> {
-    /// The underlying KV engine. It is protected by a mutex so it can be shared between txns.
     engine: Arc<Mutex<E>>,
 }
 
@@ -155,7 +304,7 @@ pub struct Status {
 
 /// An MVCC transaction.
 pub struct Transaction<E: Engine> {
-    /// The underlying engine for the transaction. Shared between transactions using a mutex.
+    /// The underlying engine, shared by all transactions.
     engine: Arc<Mutex<E>>,
     /// The transaction state.
     st: TransactionState,
@@ -165,7 +314,7 @@ pub struct Transaction<E: Engine> {
 /// is separate from Transaction to allow it to be passed around independently
 /// of the engine. There are two main motivations for this:
 ///
-/// - It can be exported via Transaction.state(), (de)serialied, and later used
+/// - It can be exported via Transaction.state(), (de)serialized, and later used
 ///   to instantiate a new functionally equivalent Transaction via
 ///   Transaction::resume(). This allows passing the transaction between the
 ///   storage engine and SQL engine (potentially running on a different node)
@@ -190,6 +339,17 @@ pub struct TransactionState {
 
 impl TransactionState {
     /// Checks whether the given version is visible to this transaction.
+    ///
+    /// Future versions, and versions belonging to active transactions as of
+    /// the start of this transaction, are never isible.
+    ///
+    /// Read-write transactions see their own writes at their version.
+    ///
+    /// Read-only queries only see versions below the transaction's version,
+    /// excluding the version itself. This is to ensure time-travel queries see
+    /// a consistent version both before and after any active transaction at
+    /// that version commits its writes. See the module documentation for
+    /// details.
     fn is_visible(&self, version: Version) -> bool {
         if self.active.get(&version).is_some() {
             false
