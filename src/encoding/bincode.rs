e30f80e36aad98ed78de5c5d3d5d93eda5ee147d
Erik Grinaker <erik@grinaker.org>
2024-04-01 03:30:00 +0200

    encoding: add Bincode (de)serializers for readers/writers

--- src/encoding/bincode.rs
+++ src/encoding/bincode.rs
@@ -8,7 +8,7 @@
 //! Confusingly, upstream bincode::(de)serialize uses different options (fixed)
 //! than DefaultOptions (variable) -- this module always uses DefaultOptions.
 
-use crate::error::Result;
+use crate::error::{Error, Result};
 
 use bincode::Options;
 
@@ -23,7 +23,35 @@ pub fn deserialize<'de, T: serde::Deserialize<'de>>(bytes: &'de [u8]) -> Result<
     Ok(bincode().deserialize(bytes)?)
 }
 
+/// Deserializes a value from a reader using Bincode.
+pub fn deserialize_from<R: std::io::Read, T: serde::de::DeserializeOwned>(reader: R) -> Result<T> {
+    Ok(bincode().deserialize_from(reader)?)
+}
+
+/// Deserializes a value from a reader using Bincode, or returns None if the
+/// reader is closed.
+pub fn maybe_deserialize_from<R: std::io::Read, T: serde::de::DeserializeOwned>(
+    reader: R,
+) -> Result<Option<T>> {
+    match bincode().deserialize_from(reader) {
+        Ok(v) => Ok(Some(v)),
+        Err(e) => match *e {
+            bincode::ErrorKind::Io(e) => match e.kind() {
+                std::io::ErrorKind::UnexpectedEof => Ok(None),
+                std::io::ErrorKind::ConnectionReset => Ok(None),
+                _ => Err(Error::from(e)),
+            },
+            _ => Err(Error::from(e)),
+        },
+    }
+}
+
 /// Serializes a value using Bincode.
 pub fn serialize<T: serde::Serialize>(value: &T) -> Result<Vec<u8>> {
     Ok(bincode().serialize(value)?)
+}
+
+/// Serializes a value to a writer using Bincode.
+pub fn serialize_into<W: std::io::Write, T: serde::Serialize>(writer: W, value: &T) -> Result<()> {
+    Ok(bincode().serialize_into(writer, value)?)
 }
