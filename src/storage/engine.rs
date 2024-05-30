cf6d48b0ffa218996ee6798fe0263e86e65afaf7
Erik Grinaker <erik@grinaker.org>
2024-05-30 18:18:42 +0200

    storage: remove unnecessary `Engine: Sync` trait bound

--- src/storage/engine.rs
+++ src/storage/engine.rs
@@ -9,7 +9,7 @@ use serde::{Deserialize, Serialize};
 /// Only supports single-threaded use since all methods (including reads) take a
 /// mutable reference -- serialized access can't be avoided anyway, since both
 /// Raft execution and file access is serial.
-pub trait Engine: std::fmt::Display + Send + Sync {
+pub trait Engine: std::fmt::Display + Send {
     /// The iterator returned by scan().
     type ScanIterator<'a>: ScanIterator + 'a
     where
