2cfaa8d925da939b68fda11a19fc8e7fd3c7c516
Erik Grinaker <erik@grinaker.org>
2024-06-09 16:20:45 +0200

    raft: increase heartbeat interval

--- src/raft/mod.rs
+++ src/raft/mod.rs
@@ -8,14 +8,13 @@ pub use message::{Envelope, Message, ReadSequence, Request, RequestID, Response,
 pub use node::{Node, NodeID, Options, Term, Ticks};
 pub use state::State;
 
-/// The interval between Raft ticks. This is the unit of time for heartbeats and
-/// elections.
+/// The interval between Raft ticks, the unit of time.
 pub const TICK_INTERVAL: std::time::Duration = std::time::Duration::from_millis(100);
 
-/// The interval between leader heartbeats, in ticks.
-const HEARTBEAT_INTERVAL: Ticks = 3;
+/// The interval between leader heartbeats in ticks.
+const HEARTBEAT_INTERVAL: Ticks = 4;
 
-/// The default election timeout range, in ticks. This is randomized in this
+/// The default election timeout range in ticks. This is randomized in this
 /// interval, to avoid election ties.
 const ELECTION_TIMEOUT_RANGE: std::ops::Range<Ticks> = 10..20;
 
